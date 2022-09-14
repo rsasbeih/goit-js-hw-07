e9eeb7fcd8329e89836cfd1e4f2439e52c42e678
Ruba Sbeih <rsasbeih@gmail.com>
2022-09-14 14:47:17 +0300

    hopefully done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 const listImg = document.querySelector(".gallery");
-import * as basicLightbox from 'basiclightbox';
+let instance;
 createImgList(galleryItems, listImg);
 
 function createImgList(items, list) {
@@ -8,23 +8,30 @@ function createImgList(items, list) {
     .map(item => {
       return `
       <li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
       <img class="gallery__image"
       src="${item.preview}"
       data-source="${item.original}"
       alt="${item.description}"/>
+   </a>
       </li>`;
     })
     .join('');
   list.innerHTML = markup;
 }
 function maximizePhoto(event) {
+  event.preventDefault();
   const selectedImg = event.target;
-  // console.log(selectedImg.dataset.original);
-  const instance = basicLightbox.create(`
-    <img src="${selectedImg.dataset.original}" width="800" height="600">
+  instance = basicLightbox.create(`
+    <img src="${selectedImg.dataset.source}" width="800" height="600">
 `);
 
   instance.show();
 }
 listImg.addEventListener("click", maximizePhoto);
 
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    instance.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const listImg = document.querySelector(".gallery");
+let lightbox;
+createImgList(galleryItems, listImg);
 
-console.log(galleryItems);
+function createImgList(items, list) {
+        const markup = items
+                .map(item => {
+                        return `
+      <li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      title="${item.description}"/>
+   </a>
+      </li>`;
+                })
+                .join('');
+        list.innerHTML = markup;
+}
+function maximizePhoto(event) {
+        event.preventDefault();
+         lightbox = new SimpleLightbox('.gallery a', { captionPosition: 'bottom', captionDelay: 250 });
+}
+listImg.addEventListener("click", maximizePhoto);
+
+// document.addEventListener("keydown", (e) => {
+//         if (e.key === "Escape") {
+//                 // instance.close();
+//         }
+// });
