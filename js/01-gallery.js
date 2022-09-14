c36ed846e2769cc98dbabfa1e1719f49097191e2
Ruba Sbeih <rsasbeih@gmail.com>
2022-09-14 12:11:21 +0300

    initial attempt

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const listImg = document.querySelector(".gallery");
+import * as basicLightbox from 'basiclightbox';
+createImgList(galleryItems, listImg);
 
-console.log(galleryItems);
+function createImgList(items, list) {
+  const markup = items
+    .map(item => {
+      return `
+      <li class="gallery__item">
+      <img class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"/>
+      </li>`;
+    })
+    .join('');
+  list.innerHTML = markup;
+}
+function maximizePhoto(event) {
+  const selectedImg = event.target;
+  // console.log(selectedImg.dataset.original);
+  const instance = basicLightbox.create(`
+    <img src="${selectedImg.dataset.original}" width="800" height="600">
+`);
+
+  instance.show();
+}
+listImg.addEventListener("click", maximizePhoto);
+
